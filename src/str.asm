ad83aaccb6d1636f359aa00c4831ce8f20fe5099
AlexCr4ckPentest <kerzhakov50@mail.ru>
2020-10-21 22:19:19 +0300

    add implementation of _strcmp

--- src/str.asm
+++ src/str.asm
@@ -36,9 +36,36 @@ _strlen:
 
 ; eax - string1
 ; ebx - string2
-; output: eax - result
+; output: eax - 1 - not equals
+;               0 - equals
 _strcmp:
-    ret
+    push ebx
+    push ecx
+
+    sub esp, 4
+    mov dword [esp-4], eax
+
+    .check_next:
+        mov eax, dword [esp-4]
+        mov al, byte [eax]
+        cmp al, byte [ebx]
+        jne .not_eq
+        add dword [esp-4], 1
+        inc ebx
+        test al, al
+        jne .check_next
+
+    xor eax, eax
+    jmp .end
+
+    .not_eq:
+        mov eax, 0x1
+
+    .end:
+        add esp, 4
+        pop ecx
+        pop ebx
+        ret
 
 
 
